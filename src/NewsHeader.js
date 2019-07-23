632e65a794d90eb9fc84c754c55aa56c055e0565
Shravan Boosanur <shravan.bv@outlook.com>
2019-07-23 13:34:27 -0700

    Week-9

--- src/NewsHeader.js
+++ src/NewsHeader.js
@@ -4,8 +4,9 @@ import styles from './NewsHeader.module.css';
 
 const NewsHeader = () => (
   <div className={styles.newsHeader}>
-    <h1>The INFO6150 News</h1>
-    <time dateTime = "Wed, June 26, 2019">Wed, June 26, 201</time>
+    
+    <h1 className={styles.title}>The INFO6150 News</h1>
+    <time dateTime = "Wed, June 26, 2019">Wed, June 26, 2019</time>
   </div>
 );
 
